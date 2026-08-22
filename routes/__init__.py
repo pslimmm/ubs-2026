0a37e1085d03162e437048750ab79e2410e44771
pslimmm <peterrsl01@gmail.com>
2026-08-22 09:13:22 +0800

    route fix

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 import routes.square
 import routes.solve
-
-import importlib
-importlib.import_module("routes.kan-cheong-delivery-driver")
+import routes.kancheong
+import routes.showdown
